92b41c71f3c67e24fdf2b70fa6cfdaf86a011b10
Marie Keita <mashakeita12@gmail.com>
2022-02-15 23:42:52 -0800

    Adjust pricing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import Hero from '../components/Hero';
-import Banner from '../components/Banner';
-import { Link } from 'react-router-dom';
-import Services from '../components/Services';
-import FeaturedRooms from '../components/FeaturedRooms';
+import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
+import Services from "../components/Services";
+import FeaturedRooms from "../components/FeaturedRooms";
 
 export default function Home() {
-    return (
-        <>
-        <Hero hero="defaultHero">
-        </Hero>
-        <Banner title="Luxurious Rooms" subtitle="deluxe rooms starting at 300$">
-                <Link to="/rooms" className="btn btn-primary">
-                      Our Rooms
-                </Link>
-        </Banner>
-        <Services/> 
-        <FeaturedRooms/>
-        </>
-
-    )
+  return (
+    <>
+      <Hero hero="defaultHero">
+        <div>
+          <video src="/videos/foodsreel.mp4" autoPlay loop muted />
+        </div>
+      </Hero>
+      <Banner title="Luxurious Rooms" subtitle="deluxe rooms starting at 300$">
+        <Link to="/rooms" className="btn btn-primary">
+          Our Rooms
+        </Link>
+      </Banner>
+      <Services />
+      <FeaturedRooms />
+    </>
+  );
 }
